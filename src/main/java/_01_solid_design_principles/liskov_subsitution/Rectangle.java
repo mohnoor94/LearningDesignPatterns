70d4bd5c0e5fc2008963552215bacf5254753b83
abukhleif <moh.noor94@gmail.com>
2019-02-15 17:15:39 +0200

    Add 'Liskov Substitution Principle' section

---
+++ src/main/java/_01_solid_design_principles/liskov_subsitution/Rectangle.java
@@ -0,0 +1,43 @@
+package _01_solid_design_principles.liskov_subsitution;
+
+public class Rectangle {
+
+    private int height;
+    private int width;
+
+    public Rectangle() {
+    }
+
+    public Rectangle(int height, int width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    public int getArea(){
+        return height *width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "height=" + height +
+                ", width=" + width +
+                '}';
+    }
+}
---
+++ src/main/java/_01_solid_design_principles/liskov_subsitution/package-info.java
@@ -0,0 +1,4 @@
+package _01_solid_design_principles.liskov_subsitution;
+
+// Liskov Substitution Principle (LSP)
+// you should be able to substitute a subclass for a base class without break the logic
